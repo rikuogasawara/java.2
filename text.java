5858eaad2ec9753a77a5c0673c07a8a12380e510
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-07-12 14:11:15 +0000

    16

--- text.java
+++ text.java
@@ -1,27 +1,69 @@
-FlyingPhone.java
+Human.java
+public abstract class Human {
+	public abstract void work();
+}
+Student.java
+public class Student extends Human {
+	@Override
+	public void work() {
+		System.out.println("勉強をします");
+	}
+}
+Programmer.java
+public class Programmer extends Human {
+	@Override
+	public void work() {
+		System.out.println("プログラムを書きます");
+	}
+
+	public void study() {
+		System.out.println("技術書を読みます");
+	}
+}
+Driver.java
+public class Driver extends Human {
+	@Override
+	public void work() {
+		System.out.println("車を運転します");
+	}
+}
+Teacher.java
+public class Teacher extends Human {
+	@Override
+	public void work() {
+		System.out.println("登壇をします");
+	}
+}
+Doctor.java
+public class Doctor extends Human {
+	@Override
+	public void work() {
+		System.out.println("診察をします");
+	}
+}
+Chapter16.java
 package practice;
 
-import extend.phone.Phone;
+public class Chapter16 {
 
-public class FlyingPhone implements Phone, Flying {
-	private int minutes;
+	public static void main(String[] args) {
+		// 5つのサブクラスのインスタンスを生成
+		Human[] humans = new Human[] {
+				new Student(),
+				new Programmer(),
+				new Driver(),
+				new Teacher(),
+				new Doctor(),
+			};
 
-	public FlyingPhone(int minutes) {
-		this.minutes = minutes;
-	}
-
-	@Override
-	public void call(String number) {
-		System.out.println(number + " に電話します。通話できるのは飛んでいる間だけです。");
-	}
-
-	@Override
-	public void fly() {
-		System.out.println(minutes + " 分間、飛びます。 ");
-	}
-
-	@Override
-	public void powerOff() {
-		Flying.super.powerOff();
+		for (Human h : humans) {
+			// workメソッドを呼び出す
+			h.work();
+            // instanceof演算子でProgrammerかどうか判定
+			if (h instanceof Programmer ) {
+                // キャストしつつメソッドを呼び出す
+				((Programmer)h).study();
+			}
+		}
 	}
 }
