1d005d193b571724c0a28d2f220c24f6fd429ddd
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-07-14 19:19:08 +0000

    18

--- text.java
+++ text.java
@@ -1,69 +1,30 @@
-Human.java
-public abstract class Human {
-	public abstract void work();
-}
-Student.java
-public class Student extends Human {
-	@Override
-	public void work() {
-		System.out.println("勉強をします");
-	}
-}
-Programmer.java
-public class Programmer extends Human {
-	@Override
-	public void work() {
-		System.out.println("プログラムを書きます");
-	}
-
-	public void study() {
-		System.out.println("技術書を読みます");
-	}
-}
-Driver.java
-public class Driver extends Human {
-	@Override
-	public void work() {
-		System.out.println("車を運転します");
-	}
-}
-Teacher.java
-public class Teacher extends Human {
-	@Override
-	public void work() {
-		System.out.println("登壇をします");
-	}
-}
-Doctor.java
-public class Doctor extends Human {
-	@Override
-	public void work() {
-		System.out.println("診察をします");
-	}
-}
-Chapter16.java
 package practice;
 
-public class Chapter16 {
+public class Chapter18_3 {
 
 	public static void main(String[] args) {
-		// 5つのサブクラスのインスタンスを生成
-		Human[] humans = new Human[] {
-				new Student(),
-				new Programmer(),
-				new Driver(),
-				new Teacher(),
-				new Doctor(),
-			};
 
-		for (Human h : humans) {
-			// workメソッドを呼び出す
-			h.work();
-            // instanceof演算子でProgrammerかどうか判定
-			if (h instanceof Programmer ) {
-                // キャストしつつメソッドを呼び出す
-				((Programmer)h).study();
-			}
-		}
+		String zip = "\\d{3}-\\d{4}";// 郵便番号の正規表現
+		String zip1 = "100-0001"; // 東京駅の郵便番号
+		String zip2 = "1000-001"; // 誤った郵便番号
+		System.out.println(zip1 + "=" + zip1.matches(zip));
+		System.out.println(zip2 + "=" + zip2.matches(zip));
+
+		String tel = "0[789]0-\\d{4}-\\d{4}"; // 携帯電話番号の正規表現
+		String tel1 = "090-1111-2222"; // 正しいフォーマットの電話番号
+		String tel2 = "090-111-12222"; // 誤ったフォーマットの電話番号
+		String tel3 = "050-1111-2222"; // 誤った値の電話番号
+		System.out.println(tel1 + "=" + tel1.matches(tel));
+		System.out.println(tel2 + "=" + tel2.matches(tel));
+		System.out.println(tel3 + "=" + tel3.matches(tel));
+
+		String time = "([01]\\d|2[0123]):[0-5]\\d"; // 時刻（24時間表記）の正規表現
+		String time1 = "23:59"; // 正しいフォーマットの時刻
+		String time2 = "23-59"; // 誤ったフォーマットの電話番号
+		String time3 = "24:00"; // 誤った値の電話番号
+		System.out.println(time1 + "=" + time1.matches(time));
+		System.out.println(time2 + "=" + time2.matches(time));
+		System.out.println(time3 + "=" + time3.matches(time));
+
 	}
 }
