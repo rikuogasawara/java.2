47d833ef1e44bf2d1b13c905b76d9d14ddc75067
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-07-19 11:58:18 +0000

    20

--- text.java
+++ text.java
@@ -1,28 +1,24 @@
-package practice;
+class Counter {
+	private final Lock lock = new ReentrantLock();
+	private long count = 0;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
-public class Chapter19_Ex2 {
-	public static void main(String[] args)   {
-		// ファイルを開き、loadメソッドでプロパティを読み込む
-		try (FileReader fr = new FileReader("C:\\pleiades\\workspace\\personal.properties")) {
-			Properties p = new Properties();
-			p.load(fr);
-
-			// 各設定内容を出力
-			System.out.println("名前:" + p.getProperty("name"));
-			System.out.println("年齢:" + p.getProperty("age"));
-			System.out.println("性別:" + p.getProperty("gender"));
-		} catch (FileNotFoundException e) {
-			System.out.println("ファイルがありませんでした");
-			// printStackTraceで例外を出力
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("エラーです");
-			// printStackTraceで例外を出力
-			e.printStackTrace();
+	public void add(long i) {
+		try {
+			lock.lock();
+//				System.out.println("足し算をします threadName = " + Thread.currentThread().getName());
+			count += i;
+		} finally {
+			lock.unlock();
 		}
+	}
+
+	public void mul(long i) {
+		try {
+			lock.lock();
+//				System.out.println("かけ算をします threadName = " + Thread.currentThread().getName());
+			count *= i;
+		} finally {
+			lock.unlock();
+		}
+	}
 }
