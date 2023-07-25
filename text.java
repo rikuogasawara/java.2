37373f6e29e194326aed6e86ecdb1a679401cddb
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-07-25 14:19:38 +0000

    応用3

--- text.java
+++ text.java
@@ -1,36 +1,28 @@
-public enum DayOfWeek {
-  Sun, Mon, Tue, Wed, Thu, Fri, Sat
-}
 
-・Chapter2.java
-```java
-public class Chapter2 {
-  public static void main(String[] args){
-		System.out.print("曜日を入力して下さい：");
-		String input = new Scanner(System.in).nextLine();
-		DayOfWeek week = DayOfWeek.valueOf(input);
-		switch (week) {
-		case Sun:
-			System.out.println("日曜日です");
-			break;
-		case Mon:
-			System.out.println("月曜日です");
-			break;
-		case Tue:
-			System.out.println("火曜日です");
-			break;
-		case Wed:
-			System.out.println("水曜日です");
-			break;
-		case Thu:
-			System.out.println("木曜日です");
-			break;
-		case Fri:
-			System.out.println("金曜日です");
-			break;
-		case Sat:
-			System.out.println("土曜日です");
-			break;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Chapter3 {
+	public static void main(String[] args) throws Exception {
+		// 当日のインスタンスからwithDayOfMonthでその月の1日のインスタンスを得る
+		LocalDate d = LocalDate.now().withDayOfMonth(1);
+
+		// lengthOfMonthでその月の最後の日が取得できる
+		final int lastDay = d.lengthOfMonth();
+		for (int i = 1; i <= lastDay; i++) {
+			// 曜日を取得
+			DayOfWeek w = d.getDayOfWeek();
+			// 曜日の文字列表現を取得
+			String wstr = w.getDisplayName(TextStyle.SHORT, Locale.getDefault());
+			System.out.print(d.getDayOfMonth() + "(" + wstr + ") ");
+			// 土曜日かどうかを判定
+			if (w == DayOfWeek.SATURDAY) {
+				System.out.println();
+			}
+
+			d = d.plusDays(1);
 		}
-  }
+	}
 }
