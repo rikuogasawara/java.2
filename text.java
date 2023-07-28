687b93f0ec889991af26ad4e86e96789be6b689b
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-07-28 21:27:22 +0000

    応用4

--- text.java
+++ text.java
@@ -1,28 +1,19 @@
+Map<Integer, String> classmates = new HashMap<>();
+classmates.put(1, "青木");
+classmates.put(2, "石坂");
+classmates.put(3, "小野田");
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
+// キーのみ
+for (Integer key : classmates.keySet()) {
+  System.out.println(key); // HashMapなので順序は不定
+}
 
-public class Chapter3 {
-	public static void main(String[] args) throws Exception {
-		// 当日のインスタンスからwithDayOfMonthでその月の1日のインスタンスを得る
-		LocalDate d = LocalDate.now().withDayOfMonth(1);
+// 値のみ
+for (String name : classmates.values()) {
+  System.out.println(name); // HashMapなので順序は不定
+}
 
-		// lengthOfMonthでその月の最後の日が取得できる
-		final int lastDay = d.lengthOfMonth();
-		for (int i = 1; i <= lastDay; i++) {
-			// 曜日を取得
-			DayOfWeek w = d.getDayOfWeek();
-			// 曜日の文字列表現を取得
-			String wstr = w.getDisplayName(TextStyle.SHORT, Locale.getDefault());
-			System.out.print(d.getDayOfMonth() + "(" + wstr + ") ");
-			// 土曜日かどうかを判定
-			if (w == DayOfWeek.SATURDAY) {
-				System.out.println();
-			}
-
-			d = d.plusDays(1);
-		}
-	}
+// キーと値のペア
+for (Map.Entry<Integer, String> classmate : classmates.entrySet()) {
+  System.out.println(classmate.getKey() + "番は" + classmate.getValue() + "さん"); // HashMapなので順序は不定
 }
