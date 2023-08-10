e31ff4db0f1d4c207c856f0d1ec6fafa4a3b53cb
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-08-10 14:58:09 +0000

    応用9

--- text.java
+++ text.java
@@ -1,25 +1,34 @@
-@FunctionalInterface
-interface Func1 {
-	boolean isOdd(int x);
+List<Fruit> fruits = new ArrayList<>();
+fruits.add(new Fruit("りんご", 7));
+fruits.add(new Fruit("みかん", 15));
+fruits.add(new Fruit("いちご", 4));
+fruits.add(new Fruit("メロン", 8));
+fruits.add(new Fruit("ぶどう", 20));
+// 10個以下に絞り込まれたリストを作る
+List<Fruit> filtered = new ArrayList<>();
+// 拡張for文でfruitsをループ
+for (Fruit fruit : fruits) {
+  // if文で10個以下に絞る
+  if (fruit.getQuantity() <= 10) {
+    // 絞り込まれたリストに追加
+    filtered.add(fruit);
+  }
 }
+// 20個ずつ追加する
+List<Fruit> ordered = new ArrayList<>();
+for (Fruit fruit : filtered) {
+  ordered.add(fruit.order(20));
+}
+// 個数が少ない順に並べ替える
+ordered.sort(new Comparator<Fruit>() {
+  @Override
+  public int compare(Fruit f1, Fruit f2) {
+    return f1.getQuantity() - f2.getQuantity();
+  }
+});
 
-@FunctionalInterface
-interface Func2 {
-	String addNamePrefix(boolean male, String name);
-}
-
-public class Chapter8 {
-	public static void main(String[] args) throws Exception {
-		Func1 f1 = x -> x % 2 == 1;
-		Func2 f2 = (male, name) -> {
-			if (male) {
-				return "Mr." + name;
-			} else {
-				return "Ms." + name;
-			}
-		};
-		System.out.println(f1.isOdd(15));
-		System.out.println(f2.addNamePrefix(true, "Smith"));
-	}
-}
+// 絞り込んで並べ替えたリストを拡張for文でループしながら出力する
+for (Fruit fruit : ordered) {
+  // 標準出力
+  System.out.println(fruit);
 }
