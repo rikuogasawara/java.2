f0200c84584a72c7062e401640e0cde9c09431fa
rikuogasawara <123867752+rikuogasawara@users.noreply.github.com>
2023-08-13 01:51:23 +0000

    応用練習

--- text.java
+++ text.java
@@ -1,34 +1,28 @@
-List<Fruit> fruits = new ArrayList<>();
-fruits.add(new Fruit("りんご", 7));
-fruits.add(new Fruit("みかん", 15));
-fruits.add(new Fruit("いちご", 4));
-fruits.add(new Fruit("メロン", 8));
-fruits.add(new Fruit("ぶどう", 20));
-// 10個以下に絞り込まれたリストを作る
-List<Fruit> filtered = new ArrayList<>();
-// 拡張for文でfruitsをループ
-for (Fruit fruit : fruits) {
-  // if文で10個以下に絞る
-  if (fruit.getQuantity() <= 10) {
-    // 絞り込まれたリストに追加
-    filtered.add(fruit);
-  }
-}
-// 20個ずつ追加する
-List<Fruit> ordered = new ArrayList<>();
-for (Fruit fruit : filtered) {
-  ordered.add(fruit.order(20));
-}
-// 個数が少ない順に並べ替える
-ordered.sort(new Comparator<Fruit>() {
-  @Override
-  public int compare(Fruit f1, Fruit f2) {
-    return f1.getQuantity() - f2.getQuantity();
-  }
-});
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <h1>form test</h1>
+        <!-- form 要素 -->
+        <form action="/form/send" method="POST">
+            お名前 <input type="text" name="name"><br>
+            誕生日 <input type="date" name="birthday"><br>
+            emailアドレス <input type="email" name="email"><br>
+            性別 <label><input type="radio" name="gender" value="0">無回答</label>
+                 <label><input type="radio" name="gender" value="1">男性</label>
+                 <label><input type="radio" name="gender" value="2">女性</label>
+                 <br>
+            趣味 <label><input type="checkbox" name="hobby" value="1">スポーツ</label>
+                 <label><input type="checkbox" name="hobby" value="2">読書</label>
+                 <label><input type="checkbox" name="hobby" value="3">ゲーム</label>
+                 <label><input type="checkbox" name="hobby" value="4">料理</label>
+                 <label><input type="checkbox" name="hobby" value="5">食</label>
+                 <label><input type="checkbox" name="hobby" value="6">旅行</label>
+                 <br>
 
-// 絞り込んで並べ替えたリストを拡張for文でループしながら出力する
-for (Fruit fruit : ordered) {
-  // 標準出力
-  System.out.println(fruit);
-}
+            <button>送信する</button>
+        </form>
+    </body>
+</html>
